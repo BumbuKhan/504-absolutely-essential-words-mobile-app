0a92373240a87d342e616f6994c66417c445e9ef
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-05-17 22:21:04 +0400

    Added faked list items for lessons, soon data will be fetched from API using service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, OnInit} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private items = [];
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    // faked data feeding (soon this data will come from service)
+    for (var i = 1; i <= 42; i++) {
+      this.items.push('Lesson ' + i);
+    }
+  }
 }
