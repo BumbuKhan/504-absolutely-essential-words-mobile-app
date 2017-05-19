7ff546fe2c8b3344e2c5a24c513bbee05e709851
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-05-19 18:19:00 +0400

    UseCase displaying now comes from settings

--- src/pages/lesson/lesson.ts
+++ src/pages/lesson/lesson.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import {Http} from '@angular/http';
 import {LessonsProvider} from '../../providers/lessons/lessons';
+import {Storage} from '@ionic/storage';
+import {Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-lesson',
@@ -13,10 +15,13 @@ export class LessonPage {
   private lessonId: Number;
   private lessonData = {};
   private loading = true;
+  private useCase = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public storage: Storage,
+    public platform: Platform,
     public lessonsProvider: LessonsProvider) {
 
     this.lessonId = this.navParams.data.lessonId;
@@ -27,6 +32,14 @@ export class LessonPage {
             this.lessonData = data;
             this.loading = false;
         });
+  }
+
+  ionViewWillEnter(){
+    this.platform.ready().then(() => {
+      this.storage.get('settings').then(settings => {
+        this.useCase = settings.useCase;
+      })
+    });
   }
 
   toggleFavorite($event){
