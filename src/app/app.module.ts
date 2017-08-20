1af989e09bb82f93194caef13744e888d31cd1fe
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-08-20 18:21:47 +0400

    Added basic popover for train mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {SettingsPage} from '../pages/settings/settings';
 import {AboutPage} from '../pages/about/about';
 import {FeedbackPage} from '../pages/feedback/feedback';
 import {TabsPage} from '../pages/tabs/tabs';
+import {PopoverPage} from '../pages/lesson/popover';
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
@@ -28,47 +29,49 @@ import {WordMentionPipe} from '../pipes/word-mention.pipe';
 import {WordMakeBoldPipe} from '../pipes/word-make-bold.pipe';
 
 @NgModule({
-    declarations: [
-        MyApp,
-        FavoritesPage,
-        HomePage,
-        LessonPage,
-        MentionedWordPage,
-        SettingsPage,
-        AboutPage,
-        FeedbackPage,
-        TabsPage,
-        WordMentionPipe,
-        WordMakeBoldPipe,
-    ],
-    imports: [
-        BrowserModule,
-        HttpModule,
-        FormsModule,
-        IonicModule.forRoot(MyApp),
-        IonicStorageModule.forRoot()
-    ],
-    bootstrap: [IonicApp],
-    entryComponents: [
-        MyApp,
-        FavoritesPage,
-        HomePage,
-        LessonPage,
-        MentionedWordPage,
-        SettingsPage,
-        AboutPage,
-        FeedbackPage,
-        TabsPage
-    ],
-    providers: [
-        StatusBar,
-        SplashScreen,
-        {provide: ErrorHandler, useClass: IonicErrorHandler},
-        LessonsProvider,
-        CommonProvider,
-        SettingsProvider,
-        FeedBackProvider
-    ]
+  declarations: [
+    MyApp,
+    FavoritesPage,
+    HomePage,
+    LessonPage,
+    MentionedWordPage,
+    SettingsPage,
+    AboutPage,
+    FeedbackPage,
+    TabsPage,
+    PopoverPage,
+    WordMentionPipe,
+    WordMakeBoldPipe,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    FavoritesPage,
+    HomePage,
+    LessonPage,
+    MentionedWordPage,
+    SettingsPage,
+    AboutPage,
+    FeedbackPage,
+    TabsPage,
+    PopoverPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LessonsProvider,
+    CommonProvider,
+    SettingsProvider,
+    FeedBackProvider
+  ]
 })
 export class AppModule {
 }
--- src/pages/lesson/lesson.ts
+++ src/pages/lesson/lesson.ts
@@ -1,98 +1,115 @@
-import {Component} from '@angular/core';
-import {NavParams, ModalController, Platform} from 'ionic-angular';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavParams, ModalController, Platform, PopoverController} from 'ionic-angular';
 
 import {Http} from '@angular/http';
 import {LessonsProvider} from '../../providers/lessons/lessons';
 import {SettingsProvider} from '../../providers/settings/settings';
 import {MentionedWordPage} from './word-mention';
+import {PopoverPage} from './popover';
 
 @Component({
-    selector: 'page-lesson',
-    templateUrl: 'lesson.html',
-    providers: [Http]
+  selector: 'page-lesson',
+  templateUrl: 'lesson.html',
+  providers: [Http]
 })
 export class LessonPage {
-    private lessonId: Number;
-    private lessonTitle: String;
-    private words = [];
-    private loading = true;
-    private useCase: Boolean;
+  private lessonId: Number;
+  private lessonTitle: String;
+  private words = [];
+  private loading = true;
+  private useCase: Boolean;
 
-    constructor(public navParams: NavParams,
-                public platform: Platform,
-                public settingsProvider: SettingsProvider,
-                public modalCtrl: ModalController,
-                public lessonsProvider: LessonsProvider) {
+  @ViewChild('popoverContent', {read: ElementRef}) content: ElementRef;
+  @ViewChild('popoverText', {read: ElementRef}) text: ElementRef;
 
-        this.lessonId = this.navParams.data.lessonId;
-        this.lessonTitle = this.navParams.data.lessonTitle;
-    }
+  constructor(public navParams: NavParams,
+              public platform: Platform,
+              public settingsProvider: SettingsProvider,
+              public modalCtrl: ModalController,
+              public lessonsProvider: LessonsProvider,
+              public popoverCtrl: PopoverController) {
 
-    ionViewWillEnter() {
-        this.platform.ready().then(() => {
-            this.settingsProvider.getSettings().then(settings => {
-                this.useCase = settings.useCase;
+    this.lessonId = this.navParams.data.lessonId;
+    this.lessonTitle = this.navParams.data.lessonTitle;
+  }
 
-                if (this.useCase) {
-                    setTimeout(() => {
-                        let wordMentions = document.querySelectorAll('.mentioned-word');
+  presentPopover(ev) {
 
-                        for (var i = 0; i < wordMentions.length; i++) {
-                            wordMentions[i].removeEventListener('click');
-                            wordMentions[i].addEventListener('click', (event) => {
-                                var e = e || window.event;
-                                var target = e.target || e.srcElement;
-                                var text = target.textContent || text.innerText;
+    let popover = this.popoverCtrl.create(PopoverPage, {
+      contentEle: this.content.nativeElement,
+      textEle: this.text.nativeElement
+    });
 
-                                this.openMentionedWordModal(text);
-                            })
-                        }
-                    }, 1000);
-                }
-            });
+    popover.present({
+      ev: ev
+    });
+  }
 
-            this.lessonsProvider.getWords().then(data => {
-                let curLessonsWords = data.filter(item => {
-                    return item.lesson_id == this.lessonId;
-                });
+  ionViewWillEnter() {
+    this.platform.ready().then(() => {
+      this.settingsProvider.getSettings().then(settings => {
+        this.useCase = settings.useCase;
 
-                this.words = curLessonsWords;
-                this.loading = false;
-            });
-        });
-    }
+        if (this.useCase) {
+          setTimeout(() => {
+            let wordMentions = document.querySelectorAll('.mentioned-word');
 
-    toggleFavorite(item) {
-        item.is_favorite = !item.is_favorite;
+            for (var i = 0; i < wordMentions.length; i++) {
+              wordMentions[i].removeEventListener('click');
+              wordMentions[i].addEventListener('click', (event) => {
+                var e = e || window.event;
+                var target = e.target || e.srcElement;
+                var text = target.textContent || text.innerText;
 
-        this.lessonsProvider.toggleFavoriteWord(item.id, item.is_favorite);
-    }
-
-    openMentionedWordModal(word) {
-
-        this.lessonsProvider.getWords().then((words) => {
-            var findedWordObj;
-
-            words.forEach((item) => {
-                if ((item.word.search(new RegExp(word, 'i')) != -1) || (word.search(new RegExp(item.word, 'i')) != -1)) {
-                    findedWordObj = item;
-                }
-            });
-
-            // but in which lesson this word is?
-            if (findedWordObj) {
-                this.lessonsProvider.getAllLessons().then((lessons) => {
-                    lessons.items.forEach((lesson) => {
-                        if (lesson.id == findedWordObj.lesson_id) {
-                            findedWordObj.lesson_title = lesson.title;
-                            let profileModal = this.modalCtrl.create(MentionedWordPage, {words: findedWordObj});
-                            profileModal.present();
-                        }
-                    })
-                });
-            } else {
-                console.log('no match...');
+                this.openMentionedWordModal(text);
+              })
             }
+          }, 1000);
+        }
+      });
+
+      this.lessonsProvider.getWords().then(data => {
+        let curLessonsWords = data.filter(item => {
+          return item.lesson_id == this.lessonId;
         });
-    }
+
+        this.words = curLessonsWords;
+        this.loading = false;
+      });
+    });
+  }
+
+  toggleFavorite(item) {
+    item.is_favorite = !item.is_favorite;
+
+    this.lessonsProvider.toggleFavoriteWord(item.id, item.is_favorite);
+  }
+
+  openMentionedWordModal(word) {
+
+    this.lessonsProvider.getWords().then((words) => {
+      var findedWordObj;
+
+      words.forEach((item) => {
+        if ((item.word.search(new RegExp(word, 'i')) != -1) || (word.search(new RegExp(item.word, 'i')) != -1)) {
+          findedWordObj = item;
+        }
+      });
+
+      // but in which lesson this word is?
+      if (findedWordObj) {
+        this.lessonsProvider.getAllLessons().then((lessons) => {
+          lessons.items.forEach((lesson) => {
+            if (lesson.id == findedWordObj.lesson_id) {
+              findedWordObj.lesson_title = lesson.title;
+              let profileModal = this.modalCtrl.create(MentionedWordPage, {words: findedWordObj});
+              profileModal.present();
+            }
+          })
+        });
+      } else {
+        console.log('no match...');
+      }
+    });
+  }
 }
