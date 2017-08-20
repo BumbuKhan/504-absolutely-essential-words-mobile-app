9f5d78f6147b9dfb7d1cd2d4f85afbcf5eb616de
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-08-20 19:07:17 +0400

    Added needed UI elements for training mode

--- src/pages/lesson/popover.ts
+++ src/pages/lesson/popover.ts
@@ -3,62 +3,11 @@ import {NavParams} from 'ionic-angular';
 
 @Component({
   template: `
-    <ion-list radio-group [(ngModel)]="fontFamily" (ionChange)="changeFontFamily()" class="popover-page">
-      <ion-row>
-        <ion-col>
-          <button (click)="changeFontSize('smaller')" ion-item detail-none class="text-button text-smaller">A</button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeFontSize('larger')" ion-item detail-none class="text-button text-larger">A</button>
-        </ion-col>
-      </ion-row>
-      <ion-row class="row-dots">
-        <ion-col>
-          <button ion-button="dot" (click)="changeBackground('white')" class="dot-white"
-                  [class.selected]="background == 'white'"></button>
-        </ion-col>
-        <ion-col>
-          <button ion-button="dot" (click)="changeBackground('tan')" class="dot-tan"
-                  [class.selected]="background == 'tan'"></button>
-        </ion-col>
-        <ion-col>
-          <button ion-button="dot" (click)="changeBackground('grey')" class="dot-grey"
-                  [class.selected]="background == 'grey'"></button>
-        </ion-col>
-        <ion-col>
-          <button ion-button="dot" (click)="changeBackground('black')" class="dot-black"
-                  [class.selected]="background == 'black'"></button>
-        </ion-col>
-      </ion-row>
-      <ion-item class="text-athelas">
-        <ion-label>Athelas</ion-label>
-        <ion-radio value="Athelas"></ion-radio>
-      </ion-item>
-      <ion-item class="text-charter">
-        <ion-label>Charter</ion-label>
-        <ion-radio value="Charter"></ion-radio>
-      </ion-item>
-      <ion-item class="text-iowan">
-        <ion-label>Iowan</ion-label>
-        <ion-radio value="Iowan"></ion-radio>
-      </ion-item>
-      <ion-item class="text-palatino">
-        <ion-label>Palatino</ion-label>
-        <ion-radio value="Palatino"></ion-radio>
-      </ion-item>
-      <ion-item class="text-san-francisco">
-        <ion-label>San Francisco</ion-label>
-        <ion-radio value="San Francisco"></ion-radio>
-      </ion-item>
-      <ion-item class="text-seravek">
-        <ion-label>Seravek</ion-label>
-        <ion-radio value="Seravek"></ion-radio>
-      </ion-item>
-      <ion-item class="text-times-new-roman">
-        <ion-label>Times New Roman</ion-label>
-        <ion-radio value="Times New Roman"></ion-radio>
-      </ion-item>
-    </ion-list>
+    <ion-item>
+      <ion-label>Training mode</ion-label>
+      <ion-toggle checked="false" color="secondary"></ion-toggle>
+    </ion-item>
+    <p padding>In training mode words will be invisible until you tap them</p>
   `
 })
 export class PopoverPage {
@@ -95,12 +44,12 @@ export class PopoverPage {
       this.contentEle = this.navParams.data.contentEle;
       this.textEle = this.navParams.data.textEle;
 
-      this.background = this.getColorName(this.contentEle.style.backgroundColor);
-      this.setFontFamily();
+      // this.background = this.getColorName(this.contentEle.style.backgroundColor);
+      // this.setFontFamily();
     }
   }
 
-  getColorName(background) {
+  /*getColorName(background) {
     let colorName = 'white';
 
     if (!background) return 'white';
@@ -112,23 +61,23 @@ export class PopoverPage {
     }
 
     return colorName;
-  }
+  }*/
 
-  setFontFamily() {
+  /*setFontFamily() {
     if (this.textEle.style.fontFamily) {
       this.fontFamily = this.textEle.style.fontFamily.replace(/'/g, "");
     }
-  }
+  }*/
 
-  changeBackground(color) {
+  /*changeBackground(color) {
     this.background = color;
     this.contentEle.style.backgroundColor = this.colors[color].bg;
     this.textEle.style.color = this.colors[color].fg;
-  }
+  }*/
 
-  changeFontSize(direction) {
+  /*changeFontSize(direction) {
     this.textEle.style.fontSize = direction;
-  }
+  }*/
 
   changeFontFamily() {
     if (this.fontFamily) this.textEle.style.fontFamily = this.fontFamily;
